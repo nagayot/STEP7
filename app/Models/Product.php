79d8edbedc441d3faaba727734fb064846fdd937
nagayot <nagayot0220@gmail.com>
2022-07-12 07:22:24 +0900

    検索結果の商品IDが違う問題の修正

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -16,6 +16,7 @@ class Product extends Model
     // Product.php(Model)と紐づくDBのテーブルを選択します
     protected $table = 'products';
 
+
     // 可変項目
     // productsテーブル内で、値をいじりたいカラムを書き出します
     protected $fillable = [
@@ -27,6 +28,7 @@ class Product extends Model
         'image'
     ];
 
+
     /**
      * リレーション組み
      * NOTE：製品は１つの会社のもの
@@ -36,6 +38,7 @@ class Product extends Model
     public function company() {
         return $this->belongsTo('App\Models\Company');
     }
+
 
     /**
      * 一覧表示用のデータ
@@ -58,8 +61,6 @@ class Product extends Model
                 'products.price',
                 'products.stock',
                 'products.comment',
-                // ↓を取ろうとすると、products.idをcomapnies.idが上書きしてしまうので、選択しないこと
-                // 'compaies.id',
                 'companies.company_name',
             )
 
@@ -73,6 +74,7 @@ class Product extends Model
         // オブジェクトとして扱えるようにします
         return $products;
     }
+
 
     /**
      * 入力されたキーワードで商品を検索します
@@ -95,22 +97,34 @@ class Product extends Model
         // 中身 : クエリビルダでproductsテーブル内のデータを取得します
         $products = DB::table('products')
 
-            // productsテーブルのcompany_idとcompaniesテーブルのidで、joinを使ってリレーションを組みます
-            ->join('companies', 'products.company_id', '=', 'companies.id')
+        // productsテーブルのcompany_idとcompaniesテーブルのidで、joinを使ってリレーションを組みます
+        ->join('companies', 'products.company_id', '=', 'companies.id')
 
-            // product_nameと検索欄に入力された文字が部分的に一致しているものを、productsテーブルから取得します
-            ->where('products.product_name', 'LIKE', '%'.$param.'%')
+        // select文で欲しい情報を選択します
+        ->select(
+            'products.id',
+            'products.image',
+            'products.product_name',
+            'products.price',
+            'products.stock',
+            'products.comment',
+            'companies.company_name',
+        )
 
-            // orderByを使って、productsテーブルの'id'カラムの降順で並び替えます
-            ->orderBy('products.id', 'desc')
+        // product_nameと検索欄に入力された文字が部分的に一致しているものを、productsテーブルから取得します
+        ->where('products.product_name', 'LIKE', '%'.$param.'%')
 
-            // １ページ最大5件になるようにページネーション機能を使います
-            // '/resources/views/product/lineup.blade.php'にもページネーションに関する記述をお忘れなく！
-            ->paginate(5);
+        // orderByを使って、productテーブルの'id'カラムの昇順で並び替えます
+        ->orderBy('products.id', 'asc')
+
+        // １ページ最大5件になるようにページネーション機能を使います
+        // '/resources/views/product/lineup.blade.php'にもページネーションに関する記述をお忘れなく！
+        ->paginate(5);
 
         // オブジェクトとして扱えるようにします
         return $products;
     }
+
 
     /**
      * 選択されたメーカー名に紐づくcompaniesテーブルのidで商品を検索します
@@ -130,24 +144,36 @@ class Product extends Model
 
         //  箱  : $productsという名前の変数(function同様に、中身が分かるものがよい)
         // 中身 : クエリビルダでcompaniesテーブル内のデータを取得します
-        $products = DB::table('companies')
+        $products = DB::table('products')
 
-            // companiesテーブルのidとproductsテーブルのcompany_idで、joinを使ってリレーションを組みます
-            ->join('products', 'companies.id', '=', 'products.company_id')
+        // companiesテーブルのidとproductsテーブルのcompany_idで、joinを使ってリレーションを組みます
+        ->join('companies', 'products.company_id', '=', 'companies.id')
 
-            // 選択されたメーカ名に紐づくcompany_idと一致しているものを、productsテーブルから取得します
-            ->where('products.company_id', $param)
+        // select文で欲しい情報を選択します
+        ->select(
+            'products.id',
+            'products.image',
+            'products.product_name',
+            'products.price',
+            'products.stock',
+            'products.comment',
+            'companies.company_name',
+        )
 
-            // orderByを使って、productテーブルの'created_at'カラムの昇順で並び替えます
-            ->orderBy('products.created_at', 'asc')
+        // 選択されたメーカ名に紐づくcompany_idと一致しているものを、productsテーブルから取得します
+        ->where('products.company_id', $param)
 
-            // １ページ最大5件になるようにページネーション機能を使います
-            // '/resources/views/product/lineup.blade.php'にもページネーションに関する記述をお忘れなく！
-            ->paginate(5);
+        // orderByを使って、productテーブルの'id'カラムの昇順で並び替えます
+        ->orderBy('products.id', 'asc')
+
+        // １ページ最大5件になるようにページネーション機能を使います
+        // '/resources/views/product/lineup.blade.php'にもページネーションに関する記述をお忘れなく！
+        ->paginate(5);
 
         // オブジェクトとして扱えるようにします
         return $products;
     }
+
 
     /**
      * 商品情報の詳細データ
@@ -160,28 +186,26 @@ class Product extends Model
         // 中身 : クエリビルダでproductsテーブル内のデータを取得します
         $product = DB::table('products')
 
-            // productsテーブルのcompany_idとcompaniesテーブルのidでリレーションを組みます
-            ->join('companies', 'products.company_id', '=', 'companies.id')
+        // productsテーブルのcompany_idとcompaniesテーブルのidでリレーションを組みます
+        ->join('companies', 'products.company_id', '=', 'companies.id')
 
-            // select文で、DBからとってきたい値のあるカラムを選択します
-            ->select(
-                'products.id',
-                'products.image',
-                'products.product_name',
-                'products.price',
-                'products.stock',
-                'products.comment',
-                'products.company_id',
-                // ↓を取ろうとすると、products.idをcomapnies.idが上書きしてしまうので、選択しないこと
-                // 'companies.id',
-                'companies.company_name',
-            )
+        // select文で、DBからとってきたい値のあるカラムを選択します
+        ->select(
+            'products.id',
+            'products.image',
+            'products.product_name',
+            'products.price',
+            'products.stock',
+            'products.comment',
+            'products.company_id',
+            'companies.company_name',
+        )
 
-            // productsテーブルのidと、$idの持つidの値が一致するものを探して、
-            ->where('products.id', $id)
+        // productsテーブルのidと、$idの持つidの値が一致するものを探して、
+        ->where('products.id', $id)
 
-            // その中で最初の1件を取得します
-            ->first();
+        // その中で最初の1件を取得します
+        ->first();
 
         // オブジェクトとして扱えるようにします
         return $product;
@@ -213,6 +237,7 @@ class Product extends Model
         ]);
     }
 
+
     /**
      * 商品情報の更新します
      * 
@@ -230,19 +255,20 @@ class Product extends Model
         // productsテーブルの
         DB::table('products')
 
-            // productsテーブルのidと、$paramの持つidの値が一致するものを探して、
-            ->where('id', $param['id'])
+        // productsテーブルのidと、$paramの持つidの値が一致するものを探して、
+        ->where('id', $param['id'])
 
-            // それぞれのカラムの内容(=>の左)を引数(=>の右)の値で更新します
-            ->update([
-                'company_id'   => $param['company_id'],
-                'product_name' => $param['product_name'],
-                'price'        => $param['price'],
-                'stock'        => $param['stock'],
-                'comment'      => $param['comment'],
-                'image'        => $param['image']
-            ]);
+        // それぞれのカラムの内容(=>の左)を引数(=>の右)の値で更新します
+        ->update([
+            'company_id'   => $param['company_id'],
+            'product_name' => $param['product_name'],
+            'price'        => $param['price'],
+            'stock'        => $param['stock'],
+            'comment'      => $param['comment'],
+            'image'        => $param['image']
+        ]);
     }
+
 
     /**
      * 商品情報を削除します
