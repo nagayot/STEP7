2e80983fac94eb5afd74dc3cad1a94f69e826f70
nagayot <64081393+nagayot@users.noreply.github.com>
2022-07-15 13:01:17 +0900

    コメント修正

--- database/migrations/2021_11_23_081453_add_image_to_products_table.php
+++ database/migrations/2021_11_23_081453_add_image_to_products_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-// 初回のマイグレーションでimageカラム忘れちゃった！という時は本ファイルのようなコマンドを打つことでリカバリー可能です
+// 初回のマイグレーションでimageカラム忘れちゃった！という時は、本ファイルのようなコマンドを打つことでリカバリー可能です
+// php artisan make:migration add_image_to_products_table --table=products
 class AddImageToProductsTable extends Migration
 {
     /**
@@ -15,7 +16,6 @@ class AddImageToProductsTable extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            // productsテーブルにimageカラムの追加
             // imageは入力必須ではないので、null許可をすること
             $table->string('image')->nullable();
         });
